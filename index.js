8331978bd8233ed7da410c1ab618e27e8f38f1e1
jvquintao <joaovictorqs98@gmail.com>
2023-11-05 17:34:05 -0300

    funcionamento do projeto

---
+++ index.js
@@ -0,0 +1,31 @@
+//desafio do Felipão
+let nomeHeroi = "ZéDaPokebola123"
+
+let xp = 5000
+
+if(xp <1001){
+    nivelHeroi = ("Ferro")
+}
+if(xp >1000 && xp <2001){
+    nivelHeroi = ("Bronze")
+}
+if(xp >2000 && xp <5001){
+    nivelHeroi = ("Prata")
+}
+if(xp >5000 && xp <7001){
+    nivelHeroi = ("Ouro")
+}
+if(xp >7000 && xp <8001){
+    nivelHeroi = ("Platina")
+}
+if(xp >8000 && xp <9001){
+    nivelHeroi = ("Ascendente")
+}
+if(xp >9000 && xp <10001){
+    nivelHeroi = ("Imortal")
+}
+if(xp >10000){
+    nivelHeroi = ("Radiante")
+}
+
+console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
